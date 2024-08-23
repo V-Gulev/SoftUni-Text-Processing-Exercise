4ee27ee4cf75917534fdd2b47187e09686b3b3bb
V-Gulev <velizargulev@abv.bg>
2024-08-23 17:38:53 +0300

    Text Filter

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,18 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String wordToRemove = scanner.nextLine();
+        String[] wordsToRemove = scanner.nextLine().split(", ");
         String text = scanner.nextLine();
-
-        while (text.contains(wordToRemove)) {
-            text = text.replace(wordToRemove, "");
+        for (String word  : wordsToRemove) {
+            text = text.replace(word,repeat(word));
         }
         System.out.println(text);
+    }
+    public static String repeat(String word) {
+        String result = "";
+        for (int i = 0; i < word.length(); i++) {
+            result += "*";
+        }
+        return result;
     }
 }

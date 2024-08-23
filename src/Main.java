d68299a4cb7ba1c4ae3c67a0c252b00c19066b73
V-Gulev <velizargulev@abv.bg>
2024-08-23 17:36:44 +0300

    Reverse String

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
+import java.util.*;
+
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        while (true) {
+            String line = scanner.nextLine();
+            if (line.equals("end")) {
+                break;
+            }
+            String reversed = "";
+            for (int i = line.length() - 1; i >= 0; i--) {
+                reversed += line.charAt(i);
+            }
+            System.out.printf("%s = %s%n",line,reversed);
+        }
+
+
+
+    }
 }

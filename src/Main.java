cad64576844ec7e674a54c9196ddc675d7ec4875
V-Gulev <velizargulev@abv.bg>
2024-08-26 18:00:10 +0300

    Morse Code Translator

--- src/Main.java
+++ src/Main.java
@@ -3,33 +3,46 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] keyString = scanner.nextLine().split(" ");
-        int[] key = new int[keyString.length];
 
-        for (int i = 0; i < key.length; i++) {
-            key[i] = Integer.parseInt(keyString[i]);
+        Map<String, String> morseCodeMap = new HashMap<>();
+        morseCodeMap.put(".-", "A");
+        morseCodeMap.put("-...", "B");
+        morseCodeMap.put("-.-.", "C");
+        morseCodeMap.put("-..", "D");
+        morseCodeMap.put(".", "E");
+        morseCodeMap.put("..-.", "F");
+        morseCodeMap.put("--.", "G");
+        morseCodeMap.put("....", "H");
+        morseCodeMap.put("..", "I");
+        morseCodeMap.put(".---", "J");
+        morseCodeMap.put("-.-", "K");
+        morseCodeMap.put(".-..", "L");
+        morseCodeMap.put("--", "M");
+        morseCodeMap.put("-.", "N");
+        morseCodeMap.put("---", "O");
+        morseCodeMap.put(".--.", "P");
+        morseCodeMap.put("--.-", "Q");
+        morseCodeMap.put(".-.", "R");
+        morseCodeMap.put("...", "S");
+        morseCodeMap.put("-", "T");
+        morseCodeMap.put("..-", "U");
+        morseCodeMap.put("...-", "V");
+        morseCodeMap.put(".--", "W");
+        morseCodeMap.put("-..-", "X");
+        morseCodeMap.put("-.--", "Y");
+        morseCodeMap.put("--..", "Z");
+
+        String[] morseCodeWords = scanner.nextLine().split(" \\| ");
+        StringBuilder result = new StringBuilder();
+
+        for (String word : morseCodeWords) {
+            String[] letters = word.split(" ");
+            for (String letter : letters) {
+                result.append(morseCodeMap.get(letter));
+            }
+            result.append(" ");
         }
 
-        while (true) {
-            String input = scanner.nextLine();
-            if (input.equals("find")) {
-                break;
-            }
-            StringBuilder result = new StringBuilder();
-            int index = 0;
-            for (int i = 0; i < input.length(); i++) {
-                char decrypted = (char) (input.charAt(i) - key[index]);
-                result.append(decrypted);
-                index++;
-                if (index == key.length){
-                    index = 0;
-                }
-            }
-            String type = result.substring(result.indexOf("&") + 1, result.lastIndexOf("&"));
-            String location = result.substring(result.indexOf("<") + 1, result.indexOf(">"));
-            System.out.printf("Found %s at %s%n", type, location);
-        }
-
-
+        System.out.println(result.toString().trim());
     }
 }

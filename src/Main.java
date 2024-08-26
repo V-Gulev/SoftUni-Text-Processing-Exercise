13eb1565e3a9648378357217ece17f6b892291be
V-Gulev <velizargulev@abv.bg>
2024-08-26 17:56:38 +0300

    Extract Person Information

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,15 @@
 import java.util.*;
-import java.util.stream.Stream;
-
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        StringBuilder numbers = new StringBuilder();
-        StringBuilder chars = new StringBuilder();
-        StringBuilder strings = new StringBuilder();
-        List<Character> text = new ArrayList<>();
-        for (int i = 0; i < input.length(); i++) {
-            text.add(input.charAt(i));
-        }
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < n; i++) {
+            String input = scanner.nextLine();
+            String name = input.substring(input.indexOf("@") + 1, input.indexOf("|"));
+            String age = input.substring(input.indexOf("#") + 1, input.indexOf("*"));
+            System.out.printf("%s is %s years old.%n", name, age);
 
-        for (Character ch : text) {
-            if (Character.isDigit(ch)) {
-                numbers.append(ch);
-            } else if (Character.isAlphabetic(ch)) {
-                strings.append(ch);
-            } else {
-                chars.append(ch);
-            }
         }
-        System.out.println(numbers.toString());
-        System.out.println(strings.toString());
-        System.out.println(chars.toString());
     }
 }

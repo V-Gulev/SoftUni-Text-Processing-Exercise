6d23de2e50798d8f4b365c06911bb6a7c723ca7f
V-Gulev <velizargulev@abv.bg>
2024-11-09 19:14:21 +0200

    Substring_03

---
+++ src/Substring_03.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Substring_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String wordToRemove = scanner.nextLine();
+        String text = scanner.nextLine();
+
+        while (text.contains(wordToRemove)) {
+            text = text.replace(wordToRemove, "");
+        }
+        System.out.println(text);
+    }
+}

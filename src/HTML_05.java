34a34e1ca494c98b2fd115409113514cbf6eeebe
V-Gulev <velizargulev@abv.bg>
2024-11-09 19:40:39 +0200

    HTML_05

---
+++ src/HTML_05.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class HTML_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String content = scanner.nextLine();
+        List<String> comments = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("end of comments")) {
+                break;
+            }
+            comments.add(input);
+        }
+        System.out.printf("<h1>%n    %s%n</h1>%n", title);
+        System.out.printf("<article>%n    %s%n</article>%n", content);
+        for (String j : comments) {
+            System.out.printf("<div>%n    %s%n</div>%n", j);
+        }
+    }
+}

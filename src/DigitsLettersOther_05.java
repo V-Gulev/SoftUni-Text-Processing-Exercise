f0626470db6ad08b2b4b7aa52fe97237ed6e77aa
V-Gulev <velizargulev@abv.bg>
2024-11-09 19:17:34 +0200

    DigitsLettersOther_05

---
+++ src/DigitsLettersOther_05.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DigitsLettersOther_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        StringBuilder numbers = new StringBuilder();
+        StringBuilder chars = new StringBuilder();
+        StringBuilder strings = new StringBuilder();
+        List<Character> text = new ArrayList<>();
+        for (int i = 0; i < input.length(); i++) {
+            text.add(input.charAt(i));
+        }
+
+        for (Character ch : text) {
+            if (Character.isDigit(ch)) {
+                numbers.append(ch);
+            } else if (Character.isAlphabetic(ch)) {
+                strings.append(ch);
+            } else {
+                chars.append(ch);
+            }
+        }
+        System.out.println(numbers.toString());
+        System.out.println(strings.toString());
+        System.out.println(chars.toString());
+    }
+}
